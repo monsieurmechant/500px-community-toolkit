cf84f9023f75c84201a6f1cdcd5366ab9c392ee1
monsieurmechant <adam@mechant.net>
2017-03-10 15:47:41 +0100

    Added refresh of photos/comments on loading

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Redis;
+use App\Jobs\User as J;
+use Illuminate\Http\Request;
+
 class PhotosController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
+        if (!Redis::get('photos.refreshed.recently.' . $request->user()->getAttribute('id'))) {
+            $this->dispatch(new J\FetchPhotos($request->user()->getAttribute('id')));
+            $this->dispatch(new J\FetchNewComments($request->user()));
+            Redis::setex('photos.refreshed.recently.' . $request->user()->getAttribute('id'), 15 * 60, true);
+        }
+
         return view('photos.index');
-   } 
+    }
 }
